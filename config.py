5c979ebcd861148fe92917f4dc5e0ab62421b8d9
LinfengZhangg <111541464+LinfengZhangg@users.noreply.github.com>
2022-08-22 14:07:29 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+API_URL = 'https://paper-api.alpaca.markets'
+API_KEY = 'PK7OEQKKAVJQ8EI2JWP4'
+API_SECRET = 'HuSdLlKh2HOrCcCkiBKBFrfG1YyVlhhKoEaFN1vH'
+
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = '2759'
+DB_NAME = 'etfdb'
